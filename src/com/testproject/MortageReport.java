91babaddba2c8193b10e9462d8b4f6ef6bad1b0c
UzairSamad <uzair.iu17@gmail.com>
2021-01-14 16:44:23 +0500

    Refactor NumberFormat method to a conctructor field for reuseability

--- src/com/testproject/MortageReport.java
+++ src/com/testproject/MortageReport.java
@@ -4,15 +4,17 @@ import java.text.NumberFormat;
 
 public class MortageReport {
 
+    private final NumberFormat currency;
     private MortageCalculator mortageCalculator;
 
     public MortageReport(MortageCalculator mortageCalculator) {
         this.mortageCalculator = mortageCalculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public  void printMortgage() {
         double mortgage = mortageCalculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
@@ -24,6 +26,6 @@ public class MortageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for(double balance: mortageCalculator.getRemainingBalances())
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+            System.out.println(currency.format(balance));
     }
 }
